896ede13c2e0975cf205142f889f9447995b92a8
Javier de Martín <javierdemartin@gmail.com>
2017-12-04 09:39:06 +0100

    File clean

--- tests/test_1/script.py
+++ tests/test_1/script.py
@@ -1,16 +1,12 @@
 # Multivariate Time Series Forecasting with LSTMs
 
-from pandas import read_csv
 from math import sqrt
 from sklearn.metrics import mean_squared_error
-from numpy import concatenate
 from datetime import datetime
 from matplotlib import pyplot
-from numpy import array, reshape
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import MinMaxScaler
-from pandas import DataFrame
-from pandas import concat
+from numpy import array, reshape, concatenate
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+from pandas import concat,DataFrame, read_csv
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Activation
 
@@ -23,27 +19,22 @@ def print_smth(description, x):
     print x
     print "-------------------------------------------------------------------------------------------"
 
-# load data:
-def parse(x):
-#        print datetime.strptime(x, '%Y/%m/%d %H:%M').month
-        return datetime.strptime(x, '%Y/%m/%d %H:%M')
+# Data reading
+##############
 
 dataset = read_csv(fileName + '.txt')
-#dataset = read_csv(fileName + '.txt', index_col = 0, date_parser = parse)
-
 
 # Drop unwanted columns (bike station ID, bike station name...)
 dataset.drop(dataset.columns[[0,2,5,6,7]], axis = 1, inplace = True)
 
 dataset.columns    = ['month','hour', 'weekday', 'free_bikes']
-#dataset.index.name = 'month'   
 
 print_smth("Read dataset", dataset.head())
 
+# Save the modified dataset into a new file
 dataset.to_csv(fileName + '_parsed.txt')
 
-####################
-# Data Preparation #
+# Data Preparation
 ####################
 
 # convert series to supervised learning
@@ -76,37 +67,31 @@ values  = dataset.values
 
 print_smth("Imported dataset", dataset.head())
 
-
 encoder     = LabelEncoder()                     # Encode columns that are not numbers
 values[:,2] = encoder.fit_transform(values[:,2]) # Encode WEEKDAY
 values[:,1] = encoder.fit_transform(values[:,1]) # Encode HOUR
 values      = values.astype('float32')           # Convert al values to floats
-
-scaler   = MinMaxScaler(feature_range=(0,1))
-scaled   = scaler.fit_transform(values)
-reframed = series_to_supervised(scaled,1,1)
+scaler      = MinMaxScaler(feature_range=(0,1))  # Normalize values
+scaled      = scaler.fit_transform(values)
+reframed    = series_to_supervised(scaled,1,1)
 
 # Drop columns that I don't want to predict
-# (0) Month(t-1) | (1) Weekday(t-1) | (2) Free Bikes (t-1) 
-# (3) Month(t)   | (4) Weekday(t)   | (5) Free Bikes(t)
+# (0) Month(t-1) | (1) Hour(t-1) | (2) Weekday(t-1) | (3) Free Bikes (t-1) 
+# (4) Month(t)   | (5) Hour(t)   | (6) Weekday(t)   | (7) Free Bikes (t) 
 reframed.drop(reframed.columns[[4, 5, 6]], axis = 1, inplace = True)
 
 print_smth("reframed dataset", reframed.head())
 
-values = reframed.values
-
+values     = reframed.values
 train_size = int(len(values) * 0.67) # Train on 67% test on 33%
 
-print "> Training on ", train_size, "/", len(values), " samples"
+print_smth("Training set size (samples)", train_size)
 
 train = values[0:train_size,:]
 test  = values[train_size:len(values), :]
 
-print_smth("Train array", train)
-
 train_x, train_y = train[:, :-1], train[:, -1]
-test_x, test_y = test[:, :-1], test[:, -1]
-
+test_x, test_y   = test[:, :-1], test[:, -1]
 
 # reshape input to be [samples, time_steps, features]
 train_x = train_x.reshape((train_x.shape[0], 1, train_x.shape[1]))
@@ -119,9 +104,7 @@ lstm_neurons = 30
 model = Sequential()
 model.add(LSTM(lstm_neurons, input_shape = (train_x.shape[1], train_x.shape[2])))
 model.add(Dense(train_x.shape[1]))
-#model.add(Activation('softmax'))
 model.compile(loss = 'mae', optimizer = 'adam', metrics = ['accuracy'])
-
 
 batch_size = 90
 epochs     = 30
@@ -129,7 +112,6 @@ epochs     = 30
 history = model.fit(train_x, train_y, epochs = epochs, batch_size = batch_size, validation_data = (test_x, test_y), verbose = 2, shuffle = False)
 model.save('test_1.h5')
 
-print "----------------------------"
 print model.summary()
 
 pyplot.plot(history.history['loss'], label = 'train')
@@ -154,7 +136,3 @@ print('Test RMSE: %.3f' % rmse)
 
 pyplot.title("RMSE " +  str(rmse) + " batch size " + str(batch_size) + " epochs " + str(epochs) + " LSTM N " + str(lstm_neurons))
 pyplot.savefig("loss.png")
-
-# Make predictions
-###############################
-
