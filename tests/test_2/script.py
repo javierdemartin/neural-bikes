3195c59b31d84215d2550f8f49e00b8a6ec5027d
Javier de Martín <javierdemartin@gmail.com>
2017-12-25 12:25:08 +0100

    Re-formatted and redone some parts

--- tests/test_2/script.py
+++ tests/test_2/script.py
@@ -1,5 +1,4 @@
-# neural-bikes test_2
-# javier de martin @ december 2017
+# Multivariate Time Series Forecasting with LSTMs
 
 from math import sqrt
 from sklearn.metrics import mean_squared_error
@@ -15,6 +14,10 @@ from keras.utils import to_categorical
 
 fileName = 'Zunzunegi'
 
+################################################################################
+# Function definition
+################################################################################
+
 def print_smth(description, x):
     print ""
     print description
@@ -22,22 +25,6 @@ def print_smth(description, x):
     print x
     print "-------------------------------------------------------------------------------------------"
 
-# Data reading
-#-------------
-
-dataset = read_csv(fileName + '.txt')
-
-# Drop unwanted columns (bike station ID, bike station name...)
-dataset.drop(dataset.columns[[0,2,5,6,7]], axis = 1, inplace = True)
-dataset.columns = ['month','hour', 'weekday', 'free_bikes']
-
-print_smth("Read dataset", dataset.head())
-
-# Save the modified dataset into a new file
-dataset.to_csv(fileName + '_parsed.txt')
-
-# Data Pre-processing
-#--------------------
 
 # convert series to supervised learning
 def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
@@ -63,46 +50,56 @@ def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
 		agg.dropna(inplace=True)
 	return agg
 
+################################################################################
+# Data preparation
+################################################################################
+
+# Data reading
+#--------------------------------------------------------------------------------
+
+dataset = read_csv(fileName + '.txt')
+
+# Drop unwanted columns (bike station ID, bike station name...)
+dataset.drop(dataset.columns[[0,2,5,6,7]], axis = 1, inplace = True)
+dataset.columns = ['month','hour', 'weekday', 'free_bikes']
+dataset.to_csv(fileName + '_parsed.txt')
+
+# Data Preparation
+#--------------------------------------------------------------------------------
+
 dataset = read_csv(fileName + '_parsed.txt', header=0, index_col = 0)
 values  = dataset.values
 
 print_smth("Imported dataset", dataset.head())
 
-encoder     = LabelEncoder()                     # Encode columns that are not numbers
-aux         = encoder.fit_transform(values[:,2]) # Encode WEEKDAY as an integer value
-values      = numpy.delete(values,2,1)           # Remove WEEKDAY column
-values[:,1] = encoder.fit_transform(values[:,1]) # Encode HOUR
+encoder                = LabelEncoder()                     # Encode columns that are not numbers
+integerEncoded_weekday = encoder.fit_transform(values[:,2]) # Encode WEEKDAY as an integer value
+values                 = numpy.delete(values,2,1)
+values[:,1]            = encoder.fit_transform(values[:,1]) # Encode HOUR as int
 
-print_smth("Integer encoded values", aux)
+print_smth("Integer encoded values", integerEncoded_weekday)
 
-encoded_weekday = to_categorical(aux)
+encoded_weekday      = to_categorical(integerEncoded_weekday)
+integerEncoded_bikes = to_categorical(values[:,2])
+#values               = numpy.delete(values,2,1) # Delete original BIKES column
 
 values = numpy.append(values, encoded_weekday, axis = 1)
-values = values.astype('float32') # Convert al values to floats
 
-bikes_categorical = to_categorical(values[:,2])           # Convert bikes to int
-values            = numpy.append(values, bikes_categorical, axis = 1)
-values            = numpy.delete(values,2,1)           # Remove FREE_BIKES column
+print_smth("To categorical", values)
 
-# create a new arrey w all the daata
+values      = values.astype('float32')           # Convert al values to floats
 
-#numpy.set_printoptions(threshold='nan')
-print_smth("HEHE", values[0])
-print values.shape
-
+values[:,2]      = values[:,2].astype(int)           # Convert bikes to int
 scaler      = MinMaxScaler(feature_range=(0,1))  # Normalize values
 scaled      = scaler.fit_transform(values)
+
 reframed    = series_to_supervised(scaled,1,1)
 
-print reframed.shape
-
 # Drop columns that I don't want to predict
-reframed.drop(reframed.columns[[30,31,32,33,34,35,36,37,38]], axis = 1, inplace = True)
+# (0) Month(t-1) | (1) Hour(t-1) | (2) Weekday(t-1) | (3) Free Bikes (t-1) 
+# (4) Month(t)   | (5) Hour(t)   | (6) Weekday(t)   | (7) Free Bikes (t) 
+reframed.drop(reframed.columns[[10,11,13,14,15,16,17,18,19]], axis = 1, inplace = True)
 
-
-print reframed.shape
-
-#numpy.set_printoptions(threshold='nan')
 print_smth("reframed dataset", reframed.head())
 
 values     = reframed.values
@@ -120,27 +117,29 @@ test_x, test_y   = test[:, :-1], test[:, -1]
 train_x = train_x.reshape((train_x.shape[0], 1, train_x.shape[1]))
 test_x  = test_x.reshape((test_x.shape[0], 1, test_x.shape[1]))
 
-print "Shapes\n-----------------------"
 print train_x.shape, train_y.shape, test_x.shape, test_y.shape
 
-# Neural Net parameter definitions and training
-#----------------------------------------------
+################################################################################
+# Model Training
+################################################################################
 
+# nn parameters
 lstm_neurons = 30
 batch_size   = 90
-epochs       = 5
+epochs       = 10
 
-# NN model definition
+
 model = Sequential()
 model.add(LSTM(lstm_neurons, input_shape = (train_x.shape[1], train_x.shape[2])))
-model.add(Dense(train_x.shape[1], activation = 'softmax'))
-#model.compile(loss = 'mae', optimizer = 'adam', metrics = ['accuracy'])
-
-model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+model.add(Dense(train_x.shape[1]))
+#model.add(Dense(train_x.shape[1], activation = 'softmax'))
+model.compile(loss = 'mae', optimizer = 'adam', metrics = ['accuracy'])
+#model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
 
 history = model.fit(train_x, train_y, epochs = epochs, batch_size = batch_size, validation_data = (test_x, test_y), verbose = 2, shuffle = False)
+model.save('test_1.h5')
 
-model.save('test_2.h5')
+print model.summary()
 
 min_y = min(history.history['loss'])
 max_y = max(history.history['loss'])
@@ -159,16 +158,36 @@ test_y = test_y.reshape((len(test_y), 1))
 inv_y  = concatenate((test_y, test_x[:, 1:]), axis=1)
 inv_y  = scaler.inverse_transform(inv_y)
 inv_y  = inv_y[:,0]
-
 # calculate RMSE
 rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
 print('Test RMSE: %.3f' % rmse)
 
 
-# Plot styling
-#-------------
+model.save('test_2.h5')
 
-plt.figure(figsize=(12, 9))
+################################################################################
+# Plot styling
+################################################################################
+
+
+ax = plt.subplot(111)    
+
+def prepare_plot():
+    plt.figure(figsize=(12, 9))
+
+    ax.spines["top"].set_visible(False)
+    ax.spines["bottom"].set_visible(False)    
+    ax.spines["right"].set_visible(False)    
+    ax.spines["left"].set_visible(False) 
+
+
+    ax.get_xaxis().tick_bottom()
+    ax.get_yaxis().tick_left()
+
+# Loss
+#--------------------------------------------------------------------------------
+
+prepare_plot()
 plt.plot(history.history['loss'], label = 'train', color = 'blue')
 plt.plot(history.history['val_loss'], label = 'test', color = 'teal')
 
@@ -184,19 +203,10 @@ plt.text((len(history.history['val_loss']) - 1) * 1.005,
 texto = "RMSE " +  str(rmse) + " batch size " + str(batch_size) + " epochs " + str(epochs) + " LSTM N " + str(lstm_neurons)
 plt.text(0, 0, texto , fontsize=9)
 
-ax = plt.subplot(111)    
-ax.spines["top"].set_visible(False)
-ax.spines["bottom"].set_visible(False)    
-ax.spines["right"].set_visible(False)    
-ax.spines["left"].set_visible(False) 
-
-ax.get_xaxis().tick_bottom()
-ax.get_yaxis().tick_left()
-
 x = history.history['loss']
 
-plt.xticks(numpy.arange(min(x), len(x), 1.0))
 
+plt.xticks(numpy.arange(min(x), len(x), 1.0))
 plt.tick_params(axis="both", which="both", bottom="off", top="off",
                 labelbottom="on", left="off", right="off", labelleft="on", colors = 'silver')
 
@@ -205,12 +215,16 @@ for y in numpy.linspace(min_y, max_y, 9):
 
 plt.savefig("loss.png", bbox_inches="tight")
 
+plt.savefig("loss.png")
 plt.close()
 
+#--------------------------------------------------------------------------------
+# Accuracy
+#--------------------------------------------------------------------------------
 
-#plt.title("RMSE " +  str(rmse) + " batch size " + str(batch_size) + " epochs " + str(epochs) + " LSTM N " + str(lstm_neurons))
-#plt.savefig("loss.png")
-#plt.plot(test_y)
-#plt.plot(yhat)
-#plt.savefig("acc.png")
+prepare_plot()
+plt.plot(test_y)
+plt.plot(inv_y)
+plt.plot(yhat)
+plt.savefig("acc.png")
 
