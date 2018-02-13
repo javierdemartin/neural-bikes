1b297b89832f90eded557fb2bd937ed8aa10bd8e
Javier de Martín <javierdemartin@gmail.com>
2018-02-13 14:15:18 +0100

    Updated script

--- tests/test_1/script.py
+++ tests/test_1/script.py
@@ -4,7 +4,6 @@
 #-------------------------------------------------------------------------------
 # Import the data and witouth doing any transformation predict
 
-
 ################################################################################
 # Libraries and Imports
 ################################################################################
@@ -18,11 +17,11 @@ from sklearn.preprocessing import MinMaxScaler, LabelEncoder, OneHotEncoder
 from sklearn.metrics import mean_squared_error
 from pandas import concat,DataFrame, read_csv
 from keras.models import Sequential
+from keras.utils import plot_model
 from keras.layers import Dense, LSTM, Activation
 from datetime import datetime
-from keras.utils import to_categorical
 
-stationToRead = 'AYUNTAMIENTO'
+stationToRead = 'ZUNZUNEGI'
 
 ################################################################################
 # Classes and Functions
@@ -106,73 +105,110 @@ def prepare_plot(xlabel, ylabel, min_y, max_y):
     plt.xlabel(xlabel, color = 'silver')
     plt.ylabel(ylabel, color = 'silver')
 
-    # for y in numpy.linspace(min_y, max_y, 9):
-    #     plt.plot(range(0, epochs), [y] * len(range(0, epochs)), "--", lw=0.5, color="black", alpha=0.3)
+# Calculates the number of incorrect samples
+def calculate_no_errors(predicted, real):
 
+    print predicted
+    print real
+
+    wrong_val = 0
+
+    for i in range(0, len(predicted)):
+        if predicted[i] != real[i]:
+            wrong_val += 1
+
+    print wrong_val, "/", len(predicted)
+
+
+#--------------------------------------------------------------------------------
+# File reading, drop non-relevant columns 
+#--------------------------------------------------------------------------------
+
+def read_file(fileName):
+
+    dataset = pandas.read_csv(fileName)
+    dataset.columns = ['datetime', 'weekday', 'id', 'station', 'free_bikes', 'free_docks']
+
+    # Only maintain rows for the analyzed station
+    dataset = dataset[dataset['station'].isin([stationToRead])]
+
+
+    print col.HEADER, "> Data from " + dataset['datetime'].iloc[0], " to ", dataset['datetime'].iloc[len(dataset) - 1], col.ENDC
+
+    print_smth("Read dataset", dataset.head())
+
+    dataset.drop(dataset.columns[[1,2,3,5]], axis = 1, inplace = True)
+
+    dataset = dataset.reset_index(drop = True)
+
+    values  = dataset.values
+
+    times = [x.split(" ")[1] for x in values[:,0]]
+
+    dataset['datetime'] = [datetime.strptime(x, '%Y/%m/%d %H:%M').timetuple().tm_yday for x in values[:,0]]
+
+    dataset.insert(loc = 1, column = 'time', value = times)
+
+
+    print_smth("Dataset with unwanted columns removed", dataset.head())
+
+    return dataset, dataset.values
+
+
+print col.HEADER
+print "   __            __     ___"
+print "  / /____  _____/ /_   <  /"
+print " / __/ _ \/ ___/ __/   / / "
+print "/ /_/  __(__  ) /_    / /  "
+print "\__/\___/____/\__/   /_/   ", col.ENDC
+                           
 
 ################################################################################
 # Data preparation
 ################################################################################
+print col.HEADER + "Data reading and preparation" + col.ENDC
 
-#--------------------------------------------------------------------------------
-# File reading, drop non-relevant columns and save it to a new file
-#--------------------------------------------------------------------------------
+######################
 
-dataset = pandas.read_csv('Bilbao.txt')
-print dataset.head()
-dataset.columns = ['year', 'month', 'day', 'hour', 'weekday', 'id', 'station', 'free_bikes', 'free_docks']
-# Only maintain rows for the analyzed station
-dataset = dataset[dataset['station'].isin([stationToRead])]
-
-print dataset.head()
-
-dataset.drop(dataset.columns[[0,2,5,6,8]], axis = 1, inplace = True)
-dataset = dataset.reset_index(drop = True)
-
-#--------------------------------------------------------------------------------
-# Data reading
-#--------------------------------------------------------------------------------
-
-values  = dataset.values
-
-# 4 columns (month, hour, weekday, free_bikes)
-print_smth("Dataset with unwanted columns removed", dataset.head())
+dataset, values = read_file('Bilbao.txt')
 
 #--------------------------------------------------------------------------------
 # Data encoding
 #--------------------------------------------------------------------------------
 
 encoder     = LabelEncoder()                     # Encode columns that are not numbers
-values[:,2] = encoder.fit_transform(values[:,2]) # Encode WEEKDAY as an integer value
-values[:,1] = encoder.fit_transform(values[:,1]) # Encode HOUR as int
+values[:,1] = encoder.fit_transform(values[:,1]) # Encode HOUR as an integer value
+# values[:,0] = encoder.fit_transform(values[:,0]) # Encode HOUR as int
 values      = values.astype('float32')           # Convert al values to floats
 
 scaler = MinMaxScaler(feature_range=(0,1))  # Normalize values
 scaled = scaler.fit_transform(values)
 
+print_smth("Dataset with normalized values", scaled)
 
 #--------------------------------------------------------------------------------
 # Generate the columns list for the supervised transformation
 #--------------------------------------------------------------------------------
+print col.HEADER + "> Transform a time series problem into a supervised learning one" + col.ENDC
 
 # Columns names for the transformation from time series to supervised learning
-columns = ['month', 'hour', 'weekday', 'free bikes']
+columns = ['doy', 'time', 'free bikes']
 
-# outputs 8 columns
-# month(t-1) | hour(t-1) | weekday (t-1) | free_bikes(t-1) | month(t) | ...
 reframed = series_to_supervised(columns, scaled,1,1)
+
+print_smth("Reframed dataset after converting series to supervised", reframed.head())
 
 # Drop columns that I don't want to predict, every (t) column except free_bikes(t)
 # month(t-1) | hour(t-1) | weekday (t-1) | free_bikes(t-1) | free_bikes(t) |
 #--------------------------------------------------------------------------
-reframed.drop(reframed.columns[[4,5,6]], axis=1, inplace=True)
+reframed.drop(reframed.columns[[3,4]], axis=1, inplace=True)
 
 values = reframed.values
 
 print_smth("Reframed dataset without columns that are not going to be predicted", reframed.head())
 
-train_size      = int(len(values) * 0.70) # Divide the set into training and test sets
-evaluation_size = int(len(values) * 0.1)
+train_size      = int(len(values) * 0.80) # Divide the set into training and test sets
+# evaluation_size = int(len(values) * 0.1)
 test_size       = int(len(values) * 0.20)
 
 # Divide between train and test sets
@@ -186,64 +222,64 @@ train_x, train_y = train[:, :-1], train[:, -1]
 test_x, test_y   = test[:, :-1], test[:, -1]
 evaluation_x, evaluation_y = evaluation[:, :-1], evaluation[:, -1]
 
-print_smth("Train X", train_x.shape) # (..., 4)
-print_smth("Train Y", train_y.shape) # (..., )
-print_smth("Test X", test_x.shape)   # (..., 4)
-print_smth("Test X", test_y.shape)   # (..., 4)
-print_smth("Evaluation X", evaluation_x.shape)   # (..., 4)
-print_smth("Evaluation X", evaluation_y.shape)   # (..., 4)
-
 # reshape input to be [samples, time_steps, features]
 train_x = train_x.reshape((train_x.shape[0], 1, train_x.shape[1])) # (...,1,4)
 test_x  = test_x.reshape((test_x.shape[0], 1, test_x.shape[1]))    # (...,1,4)
-evaluation_x  = evaluation_x.reshape((evaluation_x.shape[0], 1, evaluation_x.shape[1]))    # (...,1,4)
 
-print train_x.shape, train_y.shape, test_x.shape, test_y.shape, evaluation_x.shape, evaluation_y.shape
+print col.blue, "[Dimensions]> ", "Train X ", train_x.shape, "Train Y ", train_y.shape, "Test X ", test_x.shape, "Test Y ", test_y.shape, col.ENDC
+
 
 ################################################################################
 # Neural Network
 ################################################################################
 
+print col.HEADER + "Neural Network definition" + col.ENDC
+
 #--------------------------------------------------------------------------------
 # Parameters
 #--------------------------------------------------------------------------------
-lstm_neurons = 200
-batch_size   = 800
-epochs       = 15
+lstm_neurons = 100
+batch_size   = 50
+epochs       = 10
 
 #--------------------------------------------------------------------------------
 # Network definition
 #--------------------------------------------------------------------------------
 model = Sequential()
 model.add(LSTM(lstm_neurons, input_shape = (train_x.shape[1], train_x.shape[2])))
-model.add(Dense(train_x.shape[1]))
+model.add(Dense(1))
 model.compile(loss = 'mean_squared_error', optimizer = 'adam', metrics = ['accuracy'])
 
-history = model.fit(train_x, train_y, epochs = epochs, batch_size = batch_size, validation_data = (test_x, test_y), verbose = 2, shuffle = False)
+history = model.fit(train_x, train_y, epochs = epochs, batch_size = batch_size, 
+    validation_data = (test_x, test_y), verbose = 2, shuffle = False)
 model.save('test_1.h5')
 
-print_smth("Model summary", model.summary())
 
-from keras.utils import plot_model
-plot_model(model, to_file='model.png')
+plot_model(model, to_file='plots/model.png', show_shapes = True)
+print col.HEADER + "> Saved model shape to an image" + col.ENDC
+
 
 #--------------------------------------------------------------------------------
 # Make a prediction
 #--------------------------------------------------------------------------------
+
+print col.green, "............................", col.ENDC
 
 min_y = min(history.history['loss'])
 max_y = max(history.history['loss'])
 
 yhat   = model.predict(test_x)
 
-print_smth("test_x", test_x)
+print_smth("yhat", yhat)
 
 test_x = test_x.reshape((test_x.shape[0], test_x.shape[2]))
 # invert scaling for forecast
 inv_yhat = concatenate((test_x[:, :-1], yhat), axis=1)
 inv_yhat = scaler.inverse_transform(inv_yhat)
-inv_yhat = inv_yhat[:,3].astype(int) # Cast as int, bikes are integer numbers...
+inv_yhat = inv_yhat[:,2].astype(int) # Cast as int, bikes are integer numbers...
                                      # Only get the last column (predicted bikes)
+
+print_smth("inv_yhat after casting to int the bikes", inv_yhat)
 
 # invert scaling for actual
 test_y = test_y.reshape((len(test_y), 1))
@@ -251,23 +287,12 @@ test_y = test_y.reshape((len(test_y), 1))
 # Real values
 inv_y = concatenate((test_x[:, :-1], test_y), axis=1)
 inv_y = scaler.inverse_transform(inv_y)
-inv_y = inv_y[:,3].astype(int) # Cast as int, bikes are integer numbers...
+inv_y = inv_y[:,2].astype(int) # Cast as int, bikes are integer numbers...
                                # Only get the last column (real bikes)
 
 # calculate RMSE
 rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
-print('Test RMSE: %.3f' % rmse)
-
-################################################################################
-# Network Evaluation
-################################################################################
-
-# Evaluate the network once is trained, do so with new data (evaluation set)
-
-loss, accuracy = model.evaluate(evaluation_x, evaluation_y)
-
-print loss, accuracy
-
+print col.HEADER + '> Test RMSE: %.3f' % rmse + col.ENDC
 
 ################################################################################
 # Plot styling
@@ -298,14 +323,13 @@ plt.tick_params(bottom="off", top="off", labelbottom="on", left="off", right="of
 
 plt.savefig("plots/loss.png", bbox_inches="tight")
 plt.close()
+print col.HEADER + "> Loss plot saved" + col.ENDC
 
 #--------------------------------------------------------------------------------
 # Accuracy Plot
 #--------------------------------------------------------------------------------
 
-
-
-prepare_plot('epoc', 'accuracy', min_y, max_y)
+prepare_plot('epoch', 'accuracy', min_y, max_y)
 
 plt.title(texto,color="black", alpha=0.3)
 
@@ -326,6 +350,7 @@ plt.text((len(history.history['val_acc']) - 1) * 1.005,
 plt.tick_params(bottom="off", top="off", labelbottom="on", left="off", right="off", labelleft="on", colors = 'silver')
 plt.savefig("plots/acc.png", bbox_inches="tight")
 plt.close()
+print col.HEADER + "> Accuracy plot saved" + col.ENDC
 
 #--------------------------------------------------------------------------------
 # Training Plot
@@ -354,3 +379,10 @@ plt.text((len(inv_y) - 1) * 1.005,
 plt.tick_params(bottom="off", top="off", labelbottom="on", left="off", right="off", labelleft="on", colors = 'silver')
 
 plt.savefig("plots/train.png", bbox_inches="tight")
+plt.close()
+print col.HEADER + "> Training plot saved" + col.ENDC
+
+calculate_no_errors(inv_y, inv_yhat)
+
+
+
