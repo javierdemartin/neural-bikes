f10925a9b321918075f01e5885f8fb12e56a01ec
Javier de Martín <javierdemartin@gmail.com>
2018-10-24 13:25:04 +0200

    Updated file, deleted unused parameters

--- tests/test_1/final_script.py
+++ tests/test_1/final_script.py
@@ -1,8 +1,7 @@
 
-from utils import color, Utils, Data_mgmt, Neural_Model
-import numpy as np
-
-# utils.print_smth("HEH", 5)
+from utils import Utils
+from Data_mgmt import Data_mgmt
+from neural_model import Neural_Model
 
 data  = Data_mgmt()
 utils = Utils()
@@ -12,13 +11,13 @@ utils = Utils()
 # dataset, list_of_stations = data.encode_data(dataset)
 # data.stats_for_station()
 
-ret = data.iterate()
+# data.iterate()
 
-# scaled = data.scale_dataset(ret)
+# data.scale_dataset()
 
-# dato = data.supervised_learning(scaled)
+dato = data.supervised_learning()
 
-# train_x, train_y, validation_x, validation_y, test_x, test_y  = data.split_sets(dato, 0.8,0.19,0.01)
+data.split_sets(0.8, 0.19, 0.01)
 
-# m = Neural_Model(train_x, train_y, test_x, test_y, 100, validation_x, validation_y, 1000)
+# m = Neural_Model(100, 1000)
 # m.fit_model()	
