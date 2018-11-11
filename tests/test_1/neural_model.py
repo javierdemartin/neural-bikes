19035cc0394d487017505216d75fbfadaae1df4f
Javier de Martín <javierdemartin@gmail.com>
2018-11-11 18:01:42 +0100

    Limiting the learning rate using SGD layer

--- tests/test_1/neural_model.py
+++ tests/test_1/neural_model.py
@@ -1,14 +1,21 @@
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense, LSTM, Dropout, Activation
+from keras.layers import Dense, LSTM, Dropout, Activation, TimeDistributed
+from keras.optimizers import SGD
 import pickle # Saving MinMaxScaler
 from utils import Utils
 from Plotter import Plotter
 from sklearn.preprocessing import LabelEncoder
 from numpy import concatenate	
+import warnings
+warnings.filterwarnings(action='ignore', category=DeprecationWarning)
 
-train_model = True
-len_day = 144
+train_model  = True
+len_day      = 144
+lstm_neurons = 40
+
+learning_rate = 0.1	
+momentum = 0.8
 
 class Neural_Model:
 
@@ -24,9 +31,7 @@ class Neural_Model:
 		self.validation_x = np.load('validation_x.npy')
 		self.validation_y = np.load('validation_y.npy')
 
-		self.batch_size = batch_size
 		self.epochs = epochs
-		self.model = self.create_model()
 
 		print("Array shapes")
 		print("Train X " + str(self.train_x.shape))
@@ -38,126 +43,133 @@ class Neural_Model:
 
 		self.scaler = pickle.load(open("MinMaxScaler.sav", 'rb'))
 
-
-		self.batch_size = batch_size
-		# self.batch_size = int(self.train_x.shape[0])
+		# Train on batch size
+		if batch_size == 'b' :
+			self.batch_size = self.train_x.shape[0]
+		elif batch_size == 'hb':
+			self.batch_size = int(self.train_x.shape[0]/2)
+		else :
+			self.batch_size = batch_size		
 
 		self.utils = Utils()
 		self.p = Plotter()
 
-		self.hour_encoder = LabelEncoder()
-		self.hour_encoder.classes_ = np.load('debug/encoders/hour_encoder.npy')
-
+		self.hour_encoder    = LabelEncoder()
 		self.weekday_encoder = LabelEncoder()
-		self.weekday_encoder.classes_ = np.load('debug/encoders/weekday_encoder.npy')
-
 		self.station_encoder = LabelEncoder()
-		self.station_encoder.classes_ = np.load('debug/encoders/station_encoder.npy')
-		
-	def create_model(self):
 
-		lstm_neurons = 200
+		self.hour_encoder.classes_, self.weekday_encoder.classes_, self.station_encoder.classes_ = self.load_encoders()
+		self.model = self.create_model()
+
+	def load_encoders(self):
+		print("> Loaded encoders ")
+		return np.load('debug/encoders/hour_encoder.npy'), np.load('debug/encoders/weekday_encoder.npy'), np.load('debug/encoders/station_encoder.npy')
+		
+	def create_model(self):		
+
+
+
+		decay_rate = learning_rate / self.epochs
+		
 
 		model = Sequential()
-		model.add(LSTM(lstm_neurons, input_shape=(self.train_x.shape[1], self.train_x.shape[2]), stateful=False, return_sequences=True))
-		model.add(LSTM(lstm_neurons, return_sequences = True))
+		
+		# model.add(LSTM(lstm_neurons, return_sequences=True)) # input_shape=(self.train_x.shape[1], self.train_x.shape[2]), stateful=False))
+		model.add(LSTM(lstm_neurons, input_shape=(self.train_x.shape[1], self.train_x.shape[2]), return_sequences = True))
+		model.add(Dropout(0.1))
 		model.add(LSTM(lstm_neurons))
+		model.add(Dropout(0.1))
 		model.add(Dense(len_day))
-		# model.add(Activation('softmax'))
-		model.compile(loss='mae', optimizer='adam', metrics = ['mse', 'acc'])
+
+		
+		sgd = SGD(lr=learning_rate, momentum=momentum, decay=decay_rate, nesterov=False)
+
+
+		model.compile(loss='mse', optimizer=sgd, metrics = ['acc', 'mape', 'mse'])
 
 		return model
 
 	def fit_model(self):
 
+		decay_rate = learning_rate / self.epochs
+
+		note = "Model trained with " + str(self.epochs) + " epochs and batch size of " + str(self.batch_size) + "\nLearning Rate " + str(learning_rate) + ", momentum " + str(momentum) + " & decay rate " + str(decay_rate)
 		self.utils.check_and_create("plots/data/metrics")
 
 		if train_model == True:
 
-			history = self.model.fit(self.train_x, self.train_y, batch_size=self.batch_size, epochs=self.epochs, validation_data=(self.validation_x, self.validation_y), verbose=1, shuffle = True)
+			# for i in range(0,len(self.train_x[0])):
+			# 	print(str(self.train_x[1][:,4][i]) + " - " + str(self.train_y[0][i]))
+
+			history = self.model.fit(self.train_x, self.train_y, batch_size=self.batch_size, epochs=self.epochs, validation_data=(self.validation_x, self.validation_y), verbose=1, shuffle = False)
 			self.model.save('model/model.h5')  # creates a HDF5 file 'my_model.h5'
 
 			print("\a Finished model training")
 
 			title_plot = "Training & Validation Loss"
 			title_path = "training_loss"
-			note = "Model trained with " + str(self.epochs) + " epochs and batch size of " + str(self.batch_size)
+			
 
-			self.p.two_plot(history.history['loss'], history.history['val_loss'], "Epoch", "Loss", title_plot, "plots/" + title_path, note)
+			self.p.two_plot(history.history['loss'], history.history['val_loss'], "Epoch", "Loss", title_plot, "plots/" + title_path, note, "Loss", "Validation Loss")
 
 			title_plot = "Training & Validation Accuracy"
 			title_path = "training_acc"
 
-			self.p.two_plot(history.history['acc'], history.history['val_acc'], "Epoch", "accuracy", title_plot, "plots/" + title_path, note)		
+			self.p.two_plot(history.history['acc'], history.history['val_acc'], "Epoch", "accuracy", title_plot, "plots/" + title_path, note, "Accuracy", "Validation Accuracy")		
+
+			title_path = "training_mape"
+			title_plot = "Training & Validation MAPE"
+
+			self.p.two_plot(history.history['mean_absolute_percentage_error'], history.history['val_mean_absolute_percentage_error'], "Epoch", "accuracy", title_plot, "plots/" + title_path, note, "MAPE", "Validation MAPE")		
 
 			self.utils.save_array_txt("plots/data/metrics/acc", history.history['acc'])
 			self.utils.save_array_txt("plots/data/metrics/val_acc", history.history['val_acc'])
 			self.utils.save_array_txt("plots/data/metrics/loss", history.history['loss'])
 			self.utils.save_array_txt("plots/data/metrics/val_loss", history.history['val_loss'])
 
-			
 		else:
 
 			self.model = self.create_model()
 			self.model.load_weights("model/model.h5")
 			print("Loaded model from disk")
 
-		predicted_x = np.asarray([self.model.predict(self.test_x)])[0]
-
-		print("CAPULLO VALUES" + str(predicted_x.shape) + "\n" + str(predicted_x))
+		self.utils.print_warn("Predicting " + str(self.test_x.shape[0]) + " samples with the test set")
 
 
-		print("DIMENSIA " + str(self.test_x.shape))
+		predicted_test_set = np.asarray([self.model.predict(self.test_x)])[0] # Predict on test values
 
-		for i in range(0, self.test_x.shape[0]):
 
-			predicted = np.array([predicted_x[i]])
+		for i in range(0, self.test_x.shape[0] - 1):
 
-			print("CAPULLO VALUES" + str(predicted.shape) + "\n" + str(predicted))
+			self.test_xx = self.test_x[i] # Get i-th sample
+			aux = self.test_x[i+1] # Get i-th sample
 
-			predicted = predicted.reshape(predicted.shape[1],1)
+			predicted_test_set_i = np.array([predicted_test_set[i]])
+			predicted_test_set_i = predicted_test_set_i.reshape(predicted_test_set_i.shape[1],1)
 
-			self.test_xx = self.test_x[i]
-
-			print("PREDICTEDDD " + str(predicted))
-
-			print("RESHAPING TEST_X " + str(self.test_xx.shape))
-			print(self.test_xx)
-			
-
-			print("Shape TEST_X pre inverse_transform " + str(self.test_xx.shape) + "\n VALUES " + str(self.test_xx))
-			print("Shape PREDICTED pre inverse_transform " + str(predicted.shape) + "\n VALUES " + str(predicted))
-
+		
 			# # invert scaling for forecast
 			# inv_yhat = concatenate((self.test_x[:, 1:], predicted), axis=1)
-			inv_yhat = concatenate((self.test_xx[:,: self.test_xx.shape[1] - 1], predicted), axis=1)
-
-			# print("RESHAPING TEST_X FINAL " + str(self.test_x.shape))
-			# print(self.test_x)
+			inv_yhat = concatenate((self.test_xx[:,: self.test_xx.shape[1] - 1], predicted_test_set_i), axis=1)
 
 			inv_yhat = self.scaler.inverse_transform(inv_yhat)
 
-			print("PREDICTED 1 SHAPE " + str(inv_yhat.shape) + " VALUES " + str(inv_yhat))
+
+
+
+			station_name = str(self.station_encoder.inverse_transform([int(inv_yhat[0][2])])[0])
 
 			inv_y = self.scaler.inverse_transform(self.test_xx)
+			aux = self.scaler.inverse_transform(aux)
 
-			print("RESHAPING TEST_X FINAL " + str(inv_y.shape))
-			print(inv_y)
+			self.p.two_plot(inv_y[:,4].tolist(), list(map(int, inv_yhat[:,4].tolist())), "Time", "Free Bikes", "Prediction for " + station_name, "plots/" + str(i), note, "Real", "Predicted")
 
-			print(inv_y[:,4].tolist())
+			# self.p.plot(inv_y[:,4].tolist() +  aux[:,4].tolist(), "Time", "Availability", str(station_name) + "_meh", "plots/" + str(station_name) + "_meh")
+			self.p.two_plot(
+				inv_y[:,4].tolist() +  list(map(int, inv_yhat[:,4].tolist())), 
+				inv_y[:,4].tolist() +  aux[:,4].tolist(), 
+				"Time", "Free Bikes", "Prediction for " + station_name, "plots/" + str(i) + "_WTF", note, "Real", "Predicted")
 
-			self.p.two_plot(inv_y[:,4].tolist(), inv_yhat[:,4].tolist(), "xlabel", "ylabel", str(i) + "_availability", "plots/" + str(i), "note")
-
-
-		# predicted = [int(x) for x in predicted]
-
-		# self.p.plot(predicted, "xlabel", "ylabel", "Predicted_" + str(self.batch_size), "plots/")
-
-		# print("PREDICTED 1 SHAPE " + " VALUES " + str(predicted))
-
-		# print("SCALER MAXXX " + str(self.scaler.data_max_))
-
-		# self.predict_test_set()
 
 	def multiple_runs(self):
 
@@ -262,8 +274,6 @@ class Neural_Model:
 			self.utils.check_and_create("plots/test_set_predictions/" + str(station) + "/")
 			self.utils.save_array_txt("plots/test_set_predictions/" + str(station) + "/real_" + title_path, real)
 			self.utils.save_array_txt("plots/test_set_predictions/" + str(station) + "/predicted_" + title_path, predicted)
-
-
 			
 
 			for j in range(len_day):
@@ -280,27 +290,7 @@ class Neural_Model:
 
 			self.p.two_plot(real, predicted, "Error (bikes)", "Time", title_plot, "plots/test_set_predictions/" + station + "/" + title_path, note)
 
-		average_final = []
-		average_final2 = []
 
-		print("LISTA")
-		for i in average_error:
-			print(i)
-
-		for i in range(len_day):
-
-			print(str(i) + " >>>>>" + str(average_error[i]))
-
-			print("Sum " + str(sum(average_error[i])) + " Len " + str(len(average_error[i])) + " Average " + str(sum(average_error[i])/len(average_error[i])))
-
-			average_final.append(sum(average_error[i])/len(average_error[i]))
-			average_final2.append(sum(mierda[i])/len(mierda[i]))
-
-		note = "Averaged diference by hour of real and predicted value for all test set samples"
-
-		self.p.two_plot(average_final,[0.0], "Time", "No. Bikes", "Averaged Error", "ERROR", note)	
-		self.utils.save_array_txt("averaged_error", average_final)
-		self.utils.save_array_txt("averaged_error2", average_final2)
 
 	def get_bikes_from_array(self, test_x, test_y):
 
