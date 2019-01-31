7c917cbd62b70816f8e3641b611c4e7298115da4
Javier de Martín <javierdemartin@gmail.com>
2019-01-31 20:32:06 +0100

    Added missing files

---
+++ color.py
@@ -0,0 +1,9 @@
+# Colorful prints in the terminal
+class color:
+	HEADER    = '\033[95m'
+	blue      = '\033[94m'
+	green     = '\033[92m'
+	yellow    = '\033[93m'
+	FAIL      = '\033[91m'
+	ENDC      = '\033[0m'
+	BOLD      = '\033[1m'
