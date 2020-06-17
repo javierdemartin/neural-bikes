7d5e9ff6f8fac45239cabe74f2fb1dbf18e9946a
Javier de Martín Gil <javierdemartin@me.com>
2020-06-17 12:44:05 +0200

    Updated file

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# Javier de Martin Gil @ 2020
+
+
 from Data_mgmt import Data_mgmt
 from neural_model import Neural_Model
 import os
@@ -8,21 +11,20 @@ from cluster import Cluster
 # [1]: CITY
 # [2]: INFLUX DB PASSWORD
 # [3]: LOCAL DATA OR QUERY REMOTE DB
-
-cluster = Cluster(city=sys.argv[1])
-labels = cluster.do_cluster()
+ 
+# cluster = Cluster(city=sys.argv[1])
+# labels = cluster.do_cluster()
+# data  = Data_mgmt(city=sys.argv[1])
+# dataset = data.read_dataset()
 # 
-data  = Data_mgmt(city=sys.argv[1])
-dataset = data.read_dataset()
-data.iterate(dataset = dataset, cluster_data = labels)
-data.supervised_learning()
-data.split_sets(0.8, 0.15, 0.5)
+# data.iterate(dataset = dataset, cluster_data = labels)
+# data.supervised_learning()
+# data.split_sets(0.8, 0.15, 0.5)
 
 m = Neural_Model()
 m.fit_model()
 m.test_models_score()
 
-# data.prepare_tomorrow(labels)
-# data.prepare_today()
- 
-# m.tomorrow(append_to_db = False)
+dataToPredict = data.prepare_tomorrow()
+
+model.tomorrow(data = dataToPredict, append_to_db = False)
